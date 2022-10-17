c732e5550e542392a83f4bb9a9099787b331d9fa
AlexeyKiselyov <aleshakiselev123@gmail.com>
2022-10-17 20:40:32 +0300

    Add change on hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { Phonebook } from '../components/Phonebook/Phonebook';
 import { ContactList } from '../components/ContactList/ContactList';
@@ -7,77 +7,64 @@ import { Section } from '../components/Section/Section';
 import { Box } from '../components/Box';
 // ==============================
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+export const App = () => {
+  const contactsData = [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
 
-  componentDidUpdate(_, prevState){ 
-    if(prevState.contacts !== this.state.contacts){
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contacts')) ?? contactsData
+  );
 
-  componentDidMount(){
-    const localStorageGet = localStorage.getItem('contacts');
-    if(localStorageGet){
-      this.setState({
-        contacts: JSON.parse(localStorage.getItem('contacts'))
-      })
-    }
-  }
+  const [filter, setFilter] = useState('');
 
-  onAddContact = newUser => {
-    const uniqUserSearch = this.state.contacts.find(
-      ({ name }) => name === newUser.name
-    );
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const onAddContact = newUser => {
+    const uniqUserSearch = contacts.find(({ name }) => name === newUser.name);
     uniqUserSearch
       ? alert(`${uniqUserSearch.name} is already in contacts`)
-      : this.setState(prevState => ({
-          contacts: [...prevState.contacts, { ...newUser, id: nanoid() }],
-        }));
+      : setContacts(prevState => [...prevState, { ...newUser, id: nanoid() }]);
   };
 
-  onDeleteContact = e => {
+  const onDeleteContact = e => {
     const deleteById = e.target.closest('li[data-id]').dataset.id;
-    const newContacts = this.state.contacts.filter(
-      contact => contact.id !== deleteById
-    );
-    this.setState({
-      contacts: newContacts,
-    });
+    const newContacts = contacts.filter(contact => contact.id !== deleteById);
+    setContacts(newContacts);
   };
 
-  onChangeFilter = e => {
-    this.setState({
-      filter: e.target.value,
-    });
+  const onChangeFilter = e => {
+    setFilter(e.target.value);
   };
 
-  render() {
-    const { contacts, filter } = this.state;
+  return (
+    <Box
+      bg="background"
+      my={6}
+      mx="auto"
+      py={5}
+      px={6}
+      maxWidth="420px"
+      borderRadius="normal"
+      border="normal"
+    >
+      <Section title="Phonebook">
+        <Phonebook onAddContact={onAddContact} />
+      </Section>
 
-    return (
-      <Box bg="background" my={6} mx="auto" py={5} px={6} maxWidth="420px" borderRadius="normal" border="normal">
-        <Section title="Phonebook">
-          <Phonebook onAddContact={this.onAddContact} />
-        </Section>
-
-        <Section title="Contacts">
-          <Filter onChangeFilter={this.onChangeFilter} />
-          <ContactList
-            contacts={contacts}
-            filter={filter}
-            onDeleteContact={this.onDeleteContact}
-          />
-        </Section>
-      </Box>
-    );
-  }
-}
+      <Section title="Contacts">
+        <Filter onChangeFilter={onChangeFilter} />
+        <ContactList
+          contacts={contacts}
+          filter={filter}
+          onDeleteContact={onDeleteContact}
+        />
+      </Section>
+    </Box>
+  );
+};
--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,64 +1,73 @@
-import { Component } from 'react';
-import {VscAdd} from 'react-icons/vsc';
+import { useState } from 'react';
+import { VscAdd } from 'react-icons/vsc';
 import { Label, Input, Button } from './Phonebook.styled';
 // ==============================
-export class Phonebook extends Component {
-  state = {
-    name: '',
-    number: '',
+
+export const Phonebook = ({ onAddContact }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const state = {
+    name,
+    number,
   };
 
-  onInputChange = ({ target: { name, value } }) => {
-    this.setState({
-      [name]: value,
-    });
+  const onInputChange = ({ target: { name: inputName, value } }) => {
+    switch (inputName) {
+      case "name":
+        setName(value);
+        break;
+      case "number":
+        setNumber(value);
+        break;
+      default:
+        return null;
+    }   
   };
 
-  onFormSubmitAddContact = e => {
+  const onFormSubmitAddContact = e => {
     e.preventDefault();
-    this.props.onAddContact(this.state);
-    this.onFormReset();
-  };
-  onFormReset = () => {
-    this.setState({
-      name: '',
-      number: '',
-    });
+    onAddContact(state);
+    onFormReset();
   };
 
-  render() {
-    const { name, number } = this.state;
-    return (
-      <>
-        <form onSubmit={this.onFormSubmitAddContact}>
-          <Label>
-            Name
-            <Input
-              onChange={this.onInputChange}
-              type="text"
-              name="name"
-              value={name}
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-          </Label>
-          <Label>
-            Number
-            <Input
-              onChange={this.onInputChange}
-              value={number}
-              type="tel"
-              name="number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-              placeholder="123-45-67"
-            />
-          </Label>
-          <Button type="submit">Add contact <VscAdd/></Button>
-        </form>
-      </>
-    );
-  }
-}
+  const onFormReset = () => {
+    setName("");
+    setNumber("");   
+  };
+
+  return (
+    <>
+      <form onSubmit={onFormSubmitAddContact}>
+        <Label>
+          Name
+          <Input
+            onChange={onInputChange}
+            type="text"
+            name="name"
+            value={name}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </Label>
+        <Label>
+          Number
+          <Input
+            onChange={onInputChange}
+            value={number}
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            placeholder="123-45-67"
+          />
+        </Label>
+        <Button type="submit">
+          Add contact <VscAdd />
+        </Button>
+      </form>
+    </>
+  );
+};
